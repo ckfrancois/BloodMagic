47abf56ee82c6dfa5555acf764ab4fb632828e65
RasheedC131 <ra357094@ucf.edu>
2025-10-25 01:54:11 -0400

    Made Main Menu Main scene

--- blood-magic/scenes/enemy/turnzone.gd
+++ blood-magic/scenes/enemy/turnzone.gd
@@ -3,7 +3,7 @@ extends Area2D
 func _on_body_entered(body: CharacterBody2D) -> void:
 	print("Turn time!")
 	
-	var battle_scene = preload("res://battle.tscn")
+	var battle_scene = preload("res://scenes/modes/battle.tscn")
 	var battle_instance = battle_scene.instantiate()
 	
 	get_tree().root.add_child(battle_instance)
--- blood-magic/scripts/menus/main_menu.gd
+++ blood-magic/scripts/menus/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://overworld.tscn")
+	get_tree().change_scene_to_file("res://scenes/modes/overworld.tscn")
 
 
 func _on_exit_pressed() -> void:
