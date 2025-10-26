b71db58be1edc3f0c2257c89bbb9546f937c35a5
RasheedC131 <ra357094@ucf.edu>
2025-10-25 22:37:01 -0400

    cultivist and nappers added

---
+++ blood-magic/scenes/enemy/cultistnapper.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var combatActions: Array[combat]
+
+func combat_action(enemy : CharacterBody2D):
+	var rng = RandomNumberGenerator.new()
+	var weights = PackedFloat32Array([combatActions[0].base_weight, combatActions[1].base_weight, combatActions[2].base_weight])
+	return (combatActions[rng.rand_weighted(weights)])
+
+func collect_cultist():
+	
