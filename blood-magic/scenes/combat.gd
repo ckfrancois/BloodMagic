625755ede996b2d251bd07b0d430cdcb05953fbf
RasheedC131 <ra357094@ucf.edu>
2025-10-25 21:34:29 -0400

    Leader Cultist Finished

--- blood-magic/scenes/combat.gd
+++ blood-magic/scenes/combat.gd
@@ -8,6 +8,7 @@ extends Resource
 @export var heal : int = 0
 @export var recoil : int = 0
 
+
 # Set this higher or lower to determine if the AI will choose it
 # Higher = better action, Lower = worse action for the AI
 @export var base_weight : int = 100
